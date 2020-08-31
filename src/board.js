a2ed891fe1b9918437f26c58329b066abcd80c25
Christopher Miller <cmiller@yellowfruit.net>
2020-08-30 22:46:50 -0500

    add premliminary router support

--- src/board.js
+++
@@ -1,7 +0,0 @@
-export default class Board {
-    constructor() {
-        this.canvas = document.createElement("canvas");
-        this.canvas.width = 768
-        this.canvas.height = 1024
-    }
-}
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-export default class Main {
-    el: HTMLElement
-
-    constructor(el: HTMLElement) {
-      this.el = el;
-    }
-}
