aa110f83bcf176e089418988adb2df5cc50b3f4c
Christopher Miller <cmiller@yellowfruit.net>
2020-09-06 22:15:22 -0500

    disc dropping plinko

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/app.ts',
@@ -22,7 +21,6 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js'],
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
     }),
