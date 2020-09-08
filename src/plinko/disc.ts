24ee21697d861ed1e4161f26c2df27b309a24d87
Christopher Miller <cmiller@yellowfruit.net>
2020-09-07 21:28:18 -0500

    changed colors, changed board sizing

--- src/plinko/disc.ts
+++ src/plinko/disc.ts
@@ -4,6 +4,6 @@ export default function Disc(x:number, y:number, radius:number): Body {
   return Bodies.circle(x, y, radius, {
     restitution: 0.8,
     friction: 0.05,
-    render: { fillStyle: 'yellow' },
+    render: { fillStyle: 'red' },
   });
 }
--- src/plinko/plinko.ts
+++ src/plinko/plinko.ts
@@ -7,53 +7,64 @@ import Peg from './peg.ts';
 import SlotBar from './slotbar.ts';
 import Bumper from './bumper.ts';
 
-const width: number = 390;
-const height: number = 640;
-const nCol: number = 8;
-const pegGapSize: number = width / nCol;
-const pegRadius: number = 4;
+const width: number = 320;
+const height: number = 440;
+const nCol: number = 10;
+const pegRadius: number = 3;
+const pegGapSize: number = (width - (2 * nCol * pegRadius)) / nCol;
+const pegStartY: number = 0.1 * height;
+const discRadius: number = 0.99 * ((pegGapSize - (2 * pegRadius)) / 2);
 const nSlotBar:number = 8;
-const slotBarHeight: number = 60;
+const slotBarHeight: number = 2 * discRadius;
 const slotBarWidth: number = 5;
-const slotGapSize: number = (width - slotBarWidth * nSlotBar) / nSlotBar;
-const discRadius: number = 0.90 * ((((pegGapSize / 2) - pegRadius)) / 2);
+const slotGapSize: number = (width - (slotBarWidth * nSlotBar)) / nSlotBar;
+const nRow: number = (height - pegStartY - slotBarHeight - (2 * pegGapSize)) / pegGapSize;
 
 function buildBoard() : Body[] {
   const board = [];
   // build left wall, right wall, and ground
-  board.push(Bodies.rectangle(0, height / 2, 1, height, { isStatic: true, render: { fillStyle: 'transparent' } }));
-  board.push(Bodies.rectangle(width, height / 2, 1, height, { isStatic: true, render: { fillStyle: 'transparent' } }));
-  board.push(Bodies.rectangle(width / 2, height, width, 1, { isStatic: true, render: { fillStyle: 'transparent' } }));
+  board.push(Bodies.rectangle(0, height / 2, 1, height, {
+    isStatic: true,
+    render: { fillStyle: 'transparent' },
+  }));
+  board.push(Bodies.rectangle(width, height / 2, 1, height, {
+    isStatic: true,
+    render: { fillStyle: 'transparent' },
+  }));
+  board.push(Bodies.rectangle(width / 2, height, width, 1, {
+    isStatic: true,
+    render: { fillStyle: 'transparent' },
+  }));
   // build pegs and bumpers
-  const pegStartY: number = 0.1 * height;
-  const pegStartX: number = pegGapSize / 2;
-  const nRow: number = 1 + ((height - pegStartY - slotBarHeight - pegGapSize) / pegGapSize);
   for (let i:number = 0; i < nRow; i += 1) {
-    const y:number = pegStartY + i * pegGapSize;
+    const y:number = pegStartY + pegRadius + i * (pegGapSize + pegRadius);
     if (i % 2 === 0) {
       // full of pegs
+      const pegStartX: number = pegGapSize;
       for (let j:number = 0; j < nCol; j += 1) {
-        board.push(Peg(pegStartX + j * pegGapSize, y, pegRadius));
+        board.push(Peg(pegStartX + pegRadius + j * (pegGapSize + pegRadius), y, pegRadius));
       }
     } else {
       // left triangle bumper
-      const bumper: Body = Bumper((pegGapSize / 8), y, 3, (pegGapSize / 2) / Math.sqrt(3));
+      const bumperRadius = (pegGapSize / 2) * (Math.sqrt(3) / 2);
+      const bumper: Body = Bumper(bumperRadius / 2, y, 3, bumperRadius);
       Body.setAngle(bumper, 60 * (Math.PI / 180));
       board.push(bumper);
       // shifted pegs
+      const pegStartX: number = pegGapSize;
       for (let j:number = 0; j < (nCol - 1); j += 1) {
-        const x:number = pegStartX + j * pegGapSize + pegGapSize / 2;
+        const x:number = pegStartX + pegRadius + j * (pegGapSize + pegRadius) + pegGapSize / 2;
         board.push(Peg(x, y, pegRadius));
       }
       // right triangle bumper
-      board.push(Bumper(width - (pegGapSize / 8), y, 3, (pegGapSize / 2) / Math.sqrt(3)));
+      board.push(Bumper(width - bumperRadius / 2, y, 3, bumperRadius));
     }
   }
   // build slots
   const slotBarStartY: number = height - slotBarHeight + slotBarHeight / 2;
   const slotBarStartX: number = slotGapSize;
   for (let i:number = 0; i < nSlotBar; i += 1) {
-    const x:number = slotBarStartX + i * slotGapSize;
+    const x:number = slotBarStartX + slotBarWidth / 2 + i * slotGapSize;
     const y:number = slotBarStartY;
     board.push(SlotBar(x, y, slotBarWidth, slotBarHeight));
   }
@@ -84,7 +95,7 @@ export default class Plinko {
       const render = Render.create({
         canvas: this.canvas,
         options: {
-          background: '#ffb6c14f', // very light pink
+          background: 'white', // very light pink
           wireframes: false,
           hasBounds: true,
           width,
@@ -103,7 +114,9 @@ export default class Plinko {
     }
 
     addDisc(x:number, y:number) : void {
-      World.add(this.engine.world, Disc(x, y, discRadius));
+      const d = Disc(x, y, discRadius);
+      World.add(this.engine.world, d);
+      setTimeout(() => World.remove(this.engine.world, d), 20 * 1000);
     }
 
     close() : void {
