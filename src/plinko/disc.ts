5ed52d86214b77278afb7175559c2aa1a9a2e0d3
Christopher Miller <cmiller@yellowfruit.net>
2020-09-09 00:10:55 -0500

    ball drop on button click

--- src/plinko/disc.ts
+++ src/plinko/disc.ts
@@ -1,7 +1,9 @@
 import { Bodies } from 'matter-js';
 
-export default function Disc(x:number, y:number, radius:number): Body {
+export default function Disc(x:number, y:number,
+  radius:number, options: Record<string, unknown>): Body {
   return Bodies.circle(x, y, radius, {
+    ...options,
     restitution: 0.8,
     friction: 0.05,
     render: { fillStyle: 'red' },
--- src/plinko/plinko.ts
+++ src/plinko/plinko.ts
@@ -1,24 +1,25 @@
 import './plinko.scss';
 import {
-  Engine, Render, World, Body, Bodies,
+  Engine, Render, World, Body, Bodies, Events,
 } from 'matter-js';
 import Disc from './disc.ts';
 import Peg from './peg.ts';
 import SlotBar from './slotbar.ts';
 import Bumper from './bumper.ts';
 
-const width: number = 320;
-const height: number = 440;
-const nCol: number = 10;
-const pegRadius: number = 3;
+const width = 320;
+const height = 440;
+const nCol = 10;
+const pegRadius = 3;
 const pegGapSize: number = (width - (2 * nCol * pegRadius)) / nCol;
 const pegStartY: number = 0.1 * height;
 const discRadius: number = 0.99 * ((pegGapSize - (2 * pegRadius)) / 2);
-const nSlotBar:number = 8;
+const nSlotBar = 8;
 const slotBarHeight: number = 2 * discRadius;
-const slotBarWidth: number = 5;
+const slotBarWidth = 5;
 const slotGapSize: number = (width - (slotBarWidth * nSlotBar)) / nSlotBar;
 const nRow: number = (height - pegStartY - slotBarHeight - (2 * pegGapSize)) / pegGapSize;
+const slideRate = 3;
 
 function buildBoard() : Body[] {
   const board = [];
@@ -36,12 +37,12 @@ function buildBoard() : Body[] {
     render: { fillStyle: 'transparent' },
   }));
   // build pegs and bumpers
-  for (let i:number = 0; i < nRow; i += 1) {
+  for (let i = 0; i < nRow; i += 1) {
     const y:number = pegStartY + pegRadius + i * (pegGapSize + pegRadius);
     if (i % 2 === 0) {
       // full of pegs
       const pegStartX: number = pegGapSize;
-      for (let j:number = 0; j < nCol; j += 1) {
+      for (let j = 0; j < nCol; j += 1) {
         board.push(Peg(pegStartX + pegRadius + j * (pegGapSize + pegRadius), y, pegRadius));
       }
     } else {
@@ -52,7 +53,7 @@ function buildBoard() : Body[] {
       board.push(bumper);
       // shifted pegs
       const pegStartX: number = pegGapSize;
-      for (let j:number = 0; j < (nCol - 1); j += 1) {
+      for (let j = 0; j < (nCol - 1); j += 1) {
         const x:number = pegStartX + pegRadius + j * (pegGapSize + pegRadius) + pegGapSize / 2;
         board.push(Peg(x, y, pegRadius));
       }
@@ -63,12 +64,17 @@ function buildBoard() : Body[] {
   // build slots
   const slotBarStartY: number = height - slotBarHeight + slotBarHeight / 2;
   const slotBarStartX: number = slotGapSize;
-  for (let i:number = 0; i < nSlotBar; i += 1) {
+  for (let i = 0; i < nSlotBar; i += 1) {
     const x:number = slotBarStartX + slotBarWidth / 2 + i * slotGapSize;
     const y:number = slotBarStartY;
     board.push(SlotBar(x, y, slotBarWidth, slotBarHeight));
   }
   return board;
+}
+
+enum DiscDirection {
+    Right = 1,
+    Left = -1
 }
 
 export default class Plinko {
@@ -78,12 +84,25 @@ export default class Plinko {
 
     engine: Engine
 
+    discX: number
+
+    disc: Body
+
+    discDirection: DiscDirection
+
     constructor(el: HTMLElement, makeClose: (f: () => void) => void, params: URLSearchParams) {
       makeClose(this.close);
       params.has('nada');
       this.el = el;
+      // outer div
       const d = document.createElement('div');
       this.el.appendChild(d);
+      // button
+      const b = document.createElement('button');
+      b.innerHTML = 'Drop';
+      b.onclick = () => this.dropDisc();
+      d.appendChild(b);
+      // canvas for board
       this.canvas = document.createElement('canvas');
       this.canvas.width = width;
       this.canvas.height = height;
@@ -107,16 +126,44 @@ export default class Plinko {
       // create the board
       World.add(this.engine.world, buildBoard());
 
+      // create slider
+      this.discX = 0;
+      Events.on(this.engine, 'beforeUpdate', (e) => {
+        // if (e.timestamp % 100 < 5) {
+        this.slideDisc();
+        // }
+      });
+
       Engine.run(this.engine);
       Render.run(render);
-      this.addDisc(width / 2, 0);
-      setInterval(() => this.addDisc(Math.random() * width, 0), 5000);
     }
 
-    addDisc(x:number, y:number) : void {
-      const d = Disc(x, y, discRadius);
+    dropDisc() : void {
+      const d = Disc(this.discX, discRadius, discRadius, { collisionFilter: { category: 0x0002 } });
       World.add(this.engine.world, d);
       setTimeout(() => World.remove(this.engine.world, d), 20 * 1000);
+    }
+
+    slideDisc() : void {
+      if (this.discX < discRadius) {
+        this.discX = discRadius;
+        this.discDirection = DiscDirection.Right;
+      } else if (this.discX > (width - discRadius)) {
+        this.discX = width - discRadius;
+        this.discDirection = DiscDirection.Left;
+      }
+      this.discX += this.discDirection * slideRate;
+      if (!this.disc) {
+        this.disc = Disc(this.discX, discRadius, discRadius,
+          {
+            isStatic: true,
+            collisionFilter: {
+              mask: 0x0001,
+            },
+          });
+        World.add(this.engine.world, this.disc);
+      }
+      Body.setPosition(this.disc, { x: this.discX, y: discRadius });
     }
 
     close() : void {
